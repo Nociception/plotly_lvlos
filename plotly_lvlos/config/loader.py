0e969ad65c7689360fbd16c9386a4ef802b335e0
Nociception <nicolas.stoutzer@hotmail.fr>
2026-01-12 08:40:34 +0100

    new branch: only one little feature : load config.toml file, if exist ; tdd style

--- plotly_lvlos/config/loader.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from plotly_lvlos.utils.errors import ConfigError
-
-
-def load_config(path="config.toml"):
-    path_obj = Path(path)
-    if not path_obj.is_file():
-        raise ConfigError(f"Config file not found: {path}")
-    return None

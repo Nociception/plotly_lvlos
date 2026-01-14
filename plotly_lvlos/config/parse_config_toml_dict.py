55aeb90a9e911e8d5d247ff7bc522a8629f15743
Nociception <nicolas.stoutzer@hotmail.fr>
2026-01-14 07:12:29 +0100

    new test passed

--- plotly_lvlos/config/parse_config_toml_dict.py
+++ plotly_lvlos/config/parse_config_toml_dict.py
@@ -1,11 +1,11 @@
-# from plotly_lvlos.errors.errors_config import ConfigMissingSection
+from plotly_lvlos.errors.errors_config import ConfigMissingSection
+from plotly_lvlos.config.config_toml_dict_schema import CONFIG_TOML_DICT_SCHEMA
 
 
 def parse_config_toml_dict(toml_dict):
     """"""
-    pass
-    # for section in CONFIG_TOML_DICT_SCHEMA.keys():
-    #     if section not in toml_dict:
-    #         raise ConfigMissingSection
+    for section in CONFIG_TOML_DICT_SCHEMA.keys():
+        if section not in toml_dict:
+            raise ConfigMissingSection
 
-    # return True
+    return True
